3f815bac7ec152af4e1acc41ed728613debbc89b
PaulinaGomez <polin.gomez89@gmail.com>
2019-02-26 02:10:38 -0600

    ver.3

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,28 @@
 var db = require("../models");
 var axios = require("axios");
 var cheerio = require("cheerio");
+//var mongojs = require("mongojs");
+
+//var db2 = mongojs(fittoscrapedb, collections);
 
 module.exports = function(app) {
+  //funcion para traer el id y cambiar el key de saved a true
+  app.get("/saved/:id", function(req, res) {
+    //cambiar tipo de find a update
+    db.Article.findOneAndUpdate(
+      { _id: req.params.id },
+      { $push: { saved: true } }
+    )
+      .then(function(saved) {
+        res.render("saved", {
+          saved: saved
+        });
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
   //llama a todos los elementos de la base de datos que tengan saved=true
   app.get("/saved", function(req, res) {
     db.Article.find({ saved: true })
@@ -16,49 +36,23 @@ module.exports = function(app) {
       });
   });
 
-  //misma logica que el inicio modificar cuando se verifique el primer get
-  app.get("/scrapeagain", function(req, res) {
-    axios.get("https://www.nytimes.com/").then(function(response) {
-      var $ = cheerio.load(response.data);
-      var result = [];
-
-      $("div.css-6p6lnl").each(function(i, element) {
-        var title = $(element)
-          .find("h2")
-          .text();
-        var link = $(element)
-          .children("a")
-          .attr("href");
-
-        result.push({
-          title: title,
-          link: link
-        });
-      });
-      //falta probar si muestra el resultado en index
-      res.render("index", {
-        articles: result
-      });
-    });
+  //funcion para traer el id y cambiar el key de saved a false
+  app.get("/unsaved", function(req, res) {
+    db.Article.findOne({ _id: req.params.id });
   });
 
-  //funcion para traer el id y cambiar el key de saved a true
-  app.get("/saved/:id", function(req, res) {
-    //cambiar tipo de find a update
-    db.Article.find({ saved: true })
-      .then(function(allSavedArticles) {
-        res.render("saved", {
-          saved: allSavedArticles
-        });
+  //funcion para traer agregar una nota
+  //encuentra el articulo usando req.params.id,
+  //corre el metodo populate con la coleccion de note
+  //responde con el articulo con la nota agregada
+  app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+      .populate("note")
+      .then(function(dbArticle) {
+        res.json(dbArticle);
       })
       .catch(function(err) {
         res.json(err);
       });
   });
-
-  //funcion para traer el id y cambiar el key de saved a false
-  app.get("/unsaved", function(req, res) {});
-
-  //funcion para traer agregar una nota
-  app.get("/notes", function(req, res) {});
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,14 +4,15 @@ var cheerio = require("cheerio");
 
 module.exports = function(app) {
   // Routes
-  //primera ruta funciona solo con ingresar a / busca info fr nytimes.com
+  //primera ruta funciona solo con ingresar a / busca info de nytimes.com
   //crea una coleccion y guarda los datos buscados
   //guarda los datos en el array result y los inyecta en el index handlebars
+  //se utiliza dos veces, al ingresar a la página y al click de scrape new articles
   app.get("/", function(req, res) {
     axios.get("https://www.nytimes.com/").then(function(response) {
       var $ = cheerio.load(response.data);
       var result = [];
-
+      //estas key deben de coincidir con los establecidos en schemas o no se guardara el dato
       $("div.css-6p6lnl").each(function(i, element) {
         var title = $(element)
           .find("h2")
@@ -24,14 +25,14 @@ module.exports = function(app) {
           $(element)
             .children("a")
             .attr("href");
-
+        //guarda el resultado dentro del array result, nombre key + dato
         result.push({
           title: title,
           description: description,
           link: link
         });
 
-        // crea la coleccion article y le guarda el resultado de element
+        //crea la coleccion article y le guarda el resultado de element
         db.Article.create(result)
           .then(function(dbArticle) {
             // View the added result in the console
@@ -41,19 +42,24 @@ module.exports = function(app) {
             // If an error occurred, log it
             console.log(err);
           });
-        //guarda el resultado dentro del array result
       });
       //muestra el resultado en json
       /*res.json(result);*/
-      //muestra el resultado inyectado en el index handlebars
-      res.render("index", {
-        articles: result
+      //muestra el resultado inyectado en el index handlebars sin id
+      /*res.render("index", {
+        articles: result*/
+
+      //hace busqueda en db para obtener resultado de busqueda + id creado por mongoose
+      //inyecta todo el resultado en index.handlebars y deja id en btn save! :D
+      db.Article.find({}).then(function(articles) {
+        res.render("index", {
+          articles: articles
+        });
       });
     });
   });
 
   //segunda ruta para scrape again
-
   app.get("/home", function(req, res) {
     db.Article.find({}).then(function(articles) {
       res.render("index", {
